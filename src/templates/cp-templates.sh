c90d8af6e0fe28d05bc75fdb43fc5c290bbd1d1d
Bertik23 <bertikxxiii@gmail.com>
2024-06-08 17:39:20 +0200

    correct way of using templates

--- src/templates/cp-templates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf fit-pdf fit-html fit-knowledge fit-anki
-cp -r /home/kalvin/documents/ruby/fit-pdf/templates/fit-pdf fit-pdf
-cp -r /home/kalvin/documents/ruby/fit-html/templates/fit-html fit-html
-cp -r /home/kalvin/documents/ruby/fit-knowledge/templates/fit-knowledge fit-knowledge
-cp -r /home/kalvin/documents/ruby/fit-anki/templates/fit-anki fit-anki
