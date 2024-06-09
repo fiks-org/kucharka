8b3e05330bde53ae6bb23fbdd44e4a3ccbb144ac
Bertik23 <bertikxxiii@gmail.com>
2024-06-09 10:51:00 +0200

    removed debug echos

--- build-html.sh
+++ build-html.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo $(dirname $0)
 cd $(dirname $0)
 
 cp -r ../fiks-html/templates src/templates
-echo $(lsd src)
 
 docker run -it --rm -v $(pwd):/tmp/project \
   gitlab.fit.cvut.cz:5050/woowoo/woowoo:latest /bin/bash -c "/tmp/project/src/_build-docker.sh"
