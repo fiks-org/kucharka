686f682db2197ccb7cfe21bf414d4d72a2a4e918
Bertik23 <bertikxxiii@gmail.com>
2024-08-29 17:47:57 +0200

    correct name

--- build-html.sh
+++ build-html.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 cp -r ../fiks-html/templates src/templates
 
 docker run -it --rm -v $(pwd):/tmp/project \
-  gitlab.fit.cvut.cz:5050/woowoo/woowoo:latest /bin/bash -c "/tmp/project/src/_build-docker.sh"
+  gitlab.fit.cvut.cz:5050/woowoo/woowoo:latest /bin/bash -c "/tmp/project/src/_build-html.sh"
 
 rm -r src/templates
 # mv build ..
